dbf096f6042b6c2e284022a17ad28f3fe3ecdd24
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2024-04-16 15:43:42 -0400

    Add basic tracing (aka logging)
    This can get much more sophisticated (for instance different logging
    levels in different environments, etc. etc.) but for now it's good
    enough to get started.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use axum::{response::Html, routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(index));
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(TraceLayer::new_for_http());
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    println!("Listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
