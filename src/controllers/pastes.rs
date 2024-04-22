6f43dcd7776026bf3faf46ee75c846f04762f132
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2024-04-22 13:59:17 -0400

    Move db queries from controllers to models
    This commit moves code that reads from or writes to the database from
    the controller layer to the model layer of the app. It does this by
    defining methods that execute queries onto `Paste` (currently the only
    extant model). The database connection is provided to the model via
    dependency injection - each query function takes an argument of the form
    `db: &Database`.
    
    While I am very confident that getting the database logic out of the
    controller layer is the correct design decision, there were various
    possible ways to achieve that. I considered alternative designs
    including:
    
    - a "driver" or "client" struct, potentially per model, that would then
      hang on the `Database` struct, and use the struct's `Connection` to
      execute queries.
    - a distinct "queries" module containing all querying functions.
      Potentially with a more complicated series of functions for building a
      sql statement, binding or executing those statements (via a dependency
      injected db connection), and functions for specifying whether to
      marshall all, one, or an optional result. This would be pretty similar
      to (and was inspired by) the generated code produced by the
      `cornucopia` crate.
    
    Ultimately, I decided these alternatives were overcomplicating, at least
    for now. Having database interaction in close proximity to the models
    seems logical to me. And using dependency injection to get the database
    connection to querying functions, while somewhat repetitive, has the
    advantage of being extremely explicit about which model methods are
    going to act on the database, and which will not.

--- src/controllers/pastes.rs
+++ src/controllers/pastes.rs
@@ -8,26 +8,10 @@ use axum::{
     http::{header::HeaderMap, StatusCode},
     response::{IntoResponse, Redirect, Response},
 };
-use rusqlite::{params, OptionalExtension};
 use serde::Deserialize;
 
 pub async fn index(State(db): State<Database>) -> IndexPastesTemplate {
-    let pastes = db
-        .conn
-        .call(|conn| {
-            let mut statement = conn.prepare("SELECT id, text FROM pastes;")?;
-            let mut rows = statement.query(())?;
-            let mut pastes = Vec::new();
-            while let Some(row) = rows.next()? {
-                pastes.push(Paste {
-                    id: row.get(0).unwrap(),
-                    text: row.get(1).unwrap(),
-                })
-            }
-            Ok(pastes)
-        })
-        .await
-        .unwrap();
+    let pastes = Paste::all(&db).await.unwrap();
     IndexPastesTemplate { pastes }
 }
 
@@ -39,60 +23,24 @@ pub async fn new() -> NewPastesTemplate {
 pub struct CreateFormInput {
     pub text: String,
 }
-pub async fn create(State(db): State<Database>, Form(input): Form<CreateFormInput>) -> Response {
-    db.conn
-        .call(move |conn| {
-            conn.execute(
-                "INSERT INTO pastes (text) VALUES (?1);",
-                params![input.text],
-            )?;
-            Ok(())
-        })
-        .await
-        .unwrap();
 
+pub async fn create(State(db): State<Database>, Form(input): Form<CreateFormInput>) -> Response {
+    Paste::insert(&db, input.text).await.unwrap();
     Redirect::to("/pastes").into_response()
 }
 
 pub async fn show(Path(id): Path<i64>, State(db): State<Database>) -> impl IntoResponse {
-    let maybe_paste = db
-        .conn
-        .call(move |conn| {
-            Ok(conn
-                .query_row(
-                    "SELECT id, text FROM pastes WHERE id = ?1;",
-                    params![id],
-                    |row| {
-                        Ok(Paste {
-                            id: row.get(0).unwrap(),
-                            text: row.get(1).unwrap(),
-                        })
-                    },
-                )
-                .optional())
-        })
-        .await
-        .unwrap()
-        .unwrap();
-
+    let maybe_paste = Paste::find(&db, id).await.unwrap();
     let status_code = if maybe_paste.is_some() {
         StatusCode::OK
     } else {
         StatusCode::NOT_FOUND
     };
-
     (status_code, ShowPastesTemplate { maybe_paste })
 }
 
 pub async fn destroy(Path(id): Path<i64>, State(db): State<Database>) -> impl IntoResponse {
-    db.conn
-        .call(move |conn| {
-            conn.execute("DELETE FROM pastes WHERE id = ?1;", params![id])?;
-            Ok(())
-        })
-        .await
-        .unwrap();
-
+    Paste::delete(&db, id).await.unwrap();
     let mut headers = HeaderMap::new();
     headers.insert("HX-Redirect", "/pastes".parse().unwrap());
     headers
--- src/models/paste.rs
+++ src/models/paste.rs
@@ -1,5 +1,77 @@
+use crate::db::Database;
+use anyhow::Result;
+use rusqlite::named_params;
+
 #[derive(Debug)]
 pub struct Paste {
     pub id: i64,
     pub text: String,
+}
+
+impl Paste {
+    fn from_row(row: &rusqlite::Row) -> Result<Paste, tokio_rusqlite::Error> {
+        Ok(Paste {
+            id: row.get("id")?,
+            text: row.get("text")?,
+        })
+    }
+
+    pub async fn all(db: &Database) -> Result<Vec<Paste>> {
+        let pastes = db
+            .conn
+            .call(|conn| {
+                let mut statement = conn.prepare("SELECT id, text FROM pastes;")?;
+                let mut rows = statement.query([])?;
+                let mut pastes = Vec::new();
+                while let Some(row) = rows.next()? {
+                    pastes.push(Paste::from_row(row)?);
+                }
+                Ok(pastes)
+            })
+            .await?;
+
+        Ok(pastes)
+    }
+
+    pub async fn insert(db: &Database, text: String) -> Result<usize> {
+        let result = db
+            .conn
+            .call(move |conn| {
+                let mut statement = conn.prepare("INSERT INTO pastes (text) VALUES (:text);")?;
+                let result = statement.execute(named_params! {":text": text})?;
+                Ok(result)
+            })
+            .await?;
+
+        Ok(result)
+    }
+
+    pub async fn find(db: &Database, id: i64) -> Result<Option<Paste>> {
+        let maybe_paste = db
+            .conn
+            .call(move |conn| {
+                let mut statement = conn.prepare("SELECT id, text FROM pastes WHERE id = :id;")?;
+                let mut rows = statement.query(named_params! {":id": id})?;
+                if let Some(row) = rows.next()? {
+                    Ok(Some(Paste::from_row(row)?))
+                } else {
+                    Ok(None)
+                }
+            })
+            .await?;
+
+        Ok(maybe_paste)
+    }
+
+    pub async fn delete(db: &Database, id: i64) -> Result<usize> {
+        let result = db
+            .conn
+            .call(move |conn| {
+                let mut statement = conn.prepare("DELETE FROM pastes WHERE id = :id;")?;
+                let result = statement.execute(named_params! {":id": id})?;
+                Ok(result)
+            })
+            .await?;
+        Ok(result)
+    }
 }
