df8674c538c7db55ccc4956d367ca8b5bd6d99b6
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2024-05-23 15:35:51 -0400

    Cleanup some unused assets and a dead validation method

--- src/validators.rs
+++ src/validators.rs
@@ -7,13 +7,3 @@ pub fn not_empty_when_trimmed<S: AsRef<str>>(s: S) -> Result<(), ValidationError
 
     Ok(())
 }
-
-pub fn is_valid_username<S: AsRef<str>>(s: S) -> Result<(), ValidationError> {
-    if s.as_ref().trim().len() < 3 {
-        return Err(ValidationError::new(
-            "usernames must be 3 characters or longer",
-        ));
-    }
-
-    Ok(())
-}
