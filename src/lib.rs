1711c4a9b2410736c60359b06dc648a9f71358c3
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2024-05-29 14:25:51 -0400

    Add a timeout layer with 10s timeouts
    10s is a bit of an arbitrary number. But, current load testing against a
    cheap VPS has the app hitting a requests per second plateau at a point
    where p95 response times are still only 1.2s. So, I think 10s is
    generous enough and that should hopefully become more true as I optimize
    various things.
    
    Also, `TimeoutLayer` returns a 408 status, not a 504. I'm sort of unsure
    whether that's appropriate or not, but it does mean that browsers will
    retry the request. Firefox, for instance, retries 10 times. Just
    something to be aware of when testing this.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ use axum::{
     Router,
 };
 use memory_serve::{load_assets, MemoryServe};
-use tower_http::{compression::CompressionLayer, trace::TraceLayer};
+use std::time::Duration;
+use tower_http::{compression::CompressionLayer, timeout::TimeoutLayer, trace::TraceLayer};
 
 pub mod config;
 pub mod controllers;
@@ -65,6 +66,7 @@ pub fn router(db: Database) -> Router {
                 // disable failure tracing here since we'll log errors via controllers::Error
                 .on_failure(()),
         )
+        .layer(TimeoutLayer::new(Duration::from_secs(10)))
         .layer(CompressionLayer::new())
         .with_state(db)
 }
